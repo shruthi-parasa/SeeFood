94f07cb3f4cca55e28d2e55b8db997821c6a9359
dheeksham <66893654+dheeksham@users.noreply.github.com>
2023-05-20 13:11:56 -0700

    Add files via upload

---
+++ HackDavis23/HackDavis23/HackDavis23App.swift
@@ -0,0 +1,17 @@
+//
+//  HackDavis23App.swift
+//  HackDavis23
+//
+//  Created by Dheeksha Magesh on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HackDavis23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
