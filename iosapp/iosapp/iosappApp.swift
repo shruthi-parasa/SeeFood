932866c125a9e360cfd5c31497b11ec75876404e
dheeksham <66893654+dheeksham@users.noreply.github.com>
2023-05-20 13:38:15 -0700

    Add files via upload

---
+++ iosapp/iosapp/iosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosappApp.swift
+//  iosapp
+//
+//  Created by Dheeksha Magesh on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
