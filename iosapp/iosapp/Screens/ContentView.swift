cdc432b050bbc085ef91e58c0263a011aab60776
dheeksham <66893654+dheeksham@users.noreply.github.com>
2023-05-20 16:26:11 -0700

    Add files via upload

---
+++ iosapp/iosapp/Screens/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  iosapp
+//
+//  Created by Dheeksha Magesh on 5/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
